e805d8ae0ef5020fedb597f1dd4f4cdfd39b61b6
aleck <292677345@qq.com>
2020-05-16 12:04:32 -0700

    modify build.sh's

--- src/productcatalogservice/build.sh
+++ src/productcatalogservice/build.sh
@@ -9,7 +9,5 @@ dep ensure
 export GOPATH=$TMPPath
 echo $GOPATH
 
-rm -rf vendor/google.golang.org/grpc
 rm -rf vendor/github.com/AleckDarcy/reload
-cp -r $GOPATH/src/github.com/AleckDarcy/reload vendor/github.com/AleckDarcy/reload
 cp -r $GOPATH/src/github.com/AleckDarcy/reload/google.golang.org/grpc vendor/google.golang.org/grpc
